93df51d108cd87a2d9f52de8efd3fa9ea6930ad4
Daniel Garcia <a2309192@institutmontilivi.cat>
2024-03-05 19:28:08 +0100

    Main

---
+++ hi.py
@@ -0,0 +1,57 @@
+import random
+paraules = [
+    "gat", "cotxe", "taula", "llibre", "ordinador", 
+    "televisor", "casa", "arbre", "mar", "muntanya", 
+    "sol", "pluja", "cel", "aire", "terra", 
+    "pantalons", "camisa", "pantaló", "aventura", "emoció"
+]
+paraula = random.choice(paraules)
+paraula_oculta = "_" * len(paraula)
+nIntents = 5
+lletras_encertades = set()
+
+def ComprovarLletra(lletra, paraula):
+    lletra = lletra.lower()
+    if lletra in paraula.lower():
+        lletras_encertades.add(lletra)
+        return True
+    return False
+
+def ComVaLaParaula(paraula, lletras_encertades):
+    paraula_oculta = ""
+    for lletra in paraula:
+        if lletra.lower() in lletras_encertades:
+            paraula_oculta += lletra + " "
+        else:
+            paraula_oculta += "_ "
+    return paraula_oculta.strip()
+
+print("BEN VINGUT AL JOC DE L'AHORCADO O COM ES DIGUI:")
+print(f"LA PARAULA OCULTA CONTÉ {len(paraula)} paraules")
+print(ComVaLaParaula(paraula, lletras_encertades))
+
+while nIntents > 0 and "_" in paraula_oculta:
+    intent = input("Entra una lletra o la paraula sencera: ")
+    if len(intent) == 1:
+        if ComprovarLletra(intent, paraula):
+            print("HAS ENCERTAT")
+            paraula_oculta = ComVaLaParaula(paraula, lletras_encertades)
+            print(paraula_oculta)
+        else:
+            nIntents -= 1
+            print("F")
+            print(f"Tens {nIntents} intents restants.")
+            print("SEGÜENT INTENT:")
+    else:
+        if intent.lower() == paraula.lower():
+            paraula_oculta = paraula
+        else:
+            nIntents -= 1
+            print("F")
+            print(f"Tens {nIntents} intents restants.")
+            print("SEGÜENT INTENT:")
+
+if "_" not in paraula_oculta:
+    print("Felicitats! Has encertat la paraula.")
+else:
+    print("Has superat el nombre d'intents disponibles. La paraula era:", paraula)
